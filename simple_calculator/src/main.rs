df0c403a7e766180fcd30d8ef3bc00a478f121ce
Eliza Wielocha <61736185+ElizaWielocha@users.noreply.github.com>
2023-04-26 00:03:41 +0200

    added subtraction

--- simple_calculator/src/main.rs
+++ simple_calculator/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+// Dodawanie
 fn addition() {
     let mut x = String::new();
     println!("Enter first number: ");
@@ -14,12 +15,31 @@ fn addition() {
     println!("{} + {} = {:.3}", x, y, (x+y));           // wynik zaokraglam do 3 miejsc po przecinku
 }
 
+// Odejmowanie
+fn subtraction() {
+    let mut x = String::new();
+    println!("Enter first number: ");
+    io::stdin().read_line( &mut x ).ok();
+    let x: f64 = x.trim().parse().unwrap();
+
+    let mut y = String::new();
+    println!("Enter second number: ");
+    io::stdin().read_line( &mut y ).ok();
+    let y: f64 = y.trim().parse().unwrap();
+
+    println!("{} - {} = {:.3}", x, y, (x-y));           // wynik zaokraglam do 3 miejsc po przecinku
+}
+
+// Mnozenie
+fn multiplication() {
+    
+}
 
 fn main() {
     // proste menu
     println!("Functions:");
     println!("1. Addition (+)");
-    println!("2. Substraction (-)");
+    println!("2. Subtraction (-)");
     println!("3. Multiplication (*)");
     println!("4. Division (/)");
     println!("5. Compounding (^)");
@@ -28,8 +48,8 @@ fn main() {
 
     let mut key = String::new();                                    // zmienna z wartoscia funkcji kalkulatora 
     println!("Enter number of function you want to perform: ");
-    io::stdin().read_line( &mut key ).ok();                         // pobierz linię od użytkownika
-    let key: i32 = key.trim().parse().unwrap();                     // zmiana na int
+    io::stdin().read_line( &mut key ).ok();                             // pobierz linię od użytkownika
+    let key: i32 = key.trim().parse().unwrap();                             // zmiana na int
     println!("----------------------");
 
     if key == 1{
@@ -37,7 +57,8 @@ fn main() {
         addition();
     }
     else if key == 2{
-        println!("");
+        println!("-- SUBTRACTION --");
+        subtraction();
     }
     else if key == 3{
         println!("");
